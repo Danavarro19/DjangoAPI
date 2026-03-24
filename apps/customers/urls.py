5a7937c5d62e55560193e5f33a44ac772687d872
dan <danielnavarro0912@gmail.com>
2026-03-24 12:09:41 -0600

    adding customer appointment endpoints

--- apps/customers/urls.py
+++ apps/customers/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from apps.customers.views import customers, customer_detail
+from apps.customers.views import customers, customer_detail, customer_appointments, customer_appointment_detail
 
 urlpatterns = [
     path('', customers, name='all_customers'),
-    path('<int:customer_id>/', customer_detail, name='get_customer')
+    path('<int:customer_id>/', customer_detail, name='get_customer'),
+    path("<int:customer_id>/appointments/", customer_appointments),
+    path("<int:customer_id>/appointments/<int:appointment_id>", customer_appointment_detail),
+
 ]
--- apps/customers/views.py
+++ apps/customers/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import get_object_or_404
 from apps.customers.models import Customer
 from apps.customers.serializers import CustomerSerializer
 
+from apps.appointments.models import Appointment
+from apps.appointments.serializers import AppointmentSerializer
 
 @api_view(["GET", "POST"])
 def customers(request):
@@ -41,3 +43,21 @@ def create_customer(request):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(["GET"])
+def customer_appointments(request, customer_id):
+    customer = get_object_or_404(Customer, pk=customer_id)
+    appointments = Appointment.objects.filter(customer=customer)
+
+    serializer = AppointmentSerializer(appointments, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def customer_appointment_detail(request, customer_id, appointment_id):
+    customer = get_object_or_404(Customer, pk=customer_id)
+    try:
+        appointment = customer.appointments.get(pk=appointment_id)
+    except Appointment.DoesNotExist:
+        return Response({"message": "No entry found"},status=status.HTTP_404_NOT_FOUND)
+    serializer = AppointmentSerializer(appointment)
+    return Response(serializer.data)
